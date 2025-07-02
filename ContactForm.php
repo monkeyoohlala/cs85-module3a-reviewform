c37532abbdb512f0d5278a829da91bca5892867e
Harold Hong <monkeyoohlala@outlook.com>
2025-07-01 20:18:15 -0700

    validation and display form functions

---
+++ ContactForm.php
@@ -0,0 +1,74 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <title>Contact Me</title>
+    </head>
+
+    <body>
+        <?php 
+
+            //checks if $data is empty
+            function validateInput($data, $fieldName) {
+                global $errorCount;
+                if (empty($data)) {
+                    echo "\"$fieldName\" is a required field.<br .?\n";
+                    ++$errorCount;
+                    $retval = "";
+                } else {
+                    $retval = trim($data);
+                    $retval = stripslashes($retval);
+                }
+                return($retval);
+            }
+
+            //
+            function validateEmail($data, $fieldName) {
+                global $errorCount;
+
+                if (empty($data)) {
+                    echo "\"$fieldName\" is a required field. <br />\n";
+                    ++$errorCount;
+                    $retval ="";
+                } else {
+                    $retval = filter_var($data, FILTER_SANITIZE_EMAIL);
+                    if (!filter_var($retval, FILTER_VALIDATE_EMAIL)) {
+                        echo "\"$fieldName\" is not a valid e-mail address. <br />\n";
+                    }
+                }
+
+                return($retval);
+            }
+
+            //
+            function displayForm($Sender, $Email, $Subject, $Message) {
+                 ?> <h2 style = "text-align:center">Contact Me</h2>
+                    <form name="contact" action="ContactForm.php" method="post">
+                        <p>Your Name:
+                            <input type="text" name="Sender" value="<?php echo $Sender; ?>" /></p>
+                        <p>Your E-mail:
+                            <input type="text" name="Email" value="<?php echo $Email; ?>" /></p>
+                        <p>Subject:
+                            <input type="text" name="Subject" value ="<?php echo $Subject; ?>" /></p>
+                        <p>Message: <br />
+                            <textarea name="Message"><?php echo $Message; ?></textarea></p>
+                        <p>
+                            <input type="reset" value="Clear Form" />&nbsp; &nbsp;
+                            <input type="submit" name="Submit" value="Send Form" /></p>
+                    </form>
+                    <?php
+            }
+
+            //variables
+            $ShowForm = TRUE;
+            $errorCount = 0;
+            $Sender = "";
+            $Email = "";
+            $Subject = "";
+            $Message = "";
+
+            
+
+        ?>
+    </body>
+</html>
